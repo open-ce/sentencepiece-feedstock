7e63ec4f8f0fb7186aba7b084e881d8f7ffd5b53
Bryant Nelson <bryant.nelson@ibm.com>
2020-09-09 14:00:27 -0500

    Initial copy.

---
+++ recipe/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# (C) Copyright IBM Corp. 2019,2020. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# *****************************************************************
+
+set -ex
+
+ARCH=`uname -p`
+if [[ "${ARCH}" == 'ppc64le' ]]; then
+    ARCH_SO_NAME="powerpc64le"
+else
+    ARCH_SO_NAME=${ARCH}
+fi
+
+PAGE_SIZE=`getconf PAGE_SIZE`
+
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} .. -DSPM_BUILD_TEST=ON -DSPM_USE_BUILTIN_PROTOBUF=OFF -DCMAKE_AR=$GCC_AR
+make -j $(nproc)
+
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
+export LD_LIBRARY_PATH=${PREFIX}/lib:${LD_LIBRARY_PATH}
+make install
+
+cd ../python
+
+python setup.py install
+
+SYS_PYTHON_MAJOR=$(python -c "import sys;print(sys.version_info.major)")
+SYS_PYTHON_MINOR=$(python -c "import sys;print(sys.version_info.minor)")
+if [[ "${SYS_PYTHON_MINOR}" -gt '7' ]]; then
+    patchelf --page-size ${PAGE_SIZE} --set-rpath $LD_LIBRARY_PATH $PREFIX/lib/python${SYS_PYTHON_MAJOR}.${SYS_PYTHON_MINOR}/site-packages/sentencepiece-$PKG_VERSION-py${SYS_PYTHON_MAJOR}.${SYS_PYTHON_MINOR}-linux-${ARCH}.egg/_sentencepiece.cpython-${CONDA_PY}-${ARCH_SO_NAME}-linux-gnu.so
+else
+    patchelf --page-size ${PAGE_SIZE} --set-rpath $LD_LIBRARY_PATH $PREFIX/lib/python${SYS_PYTHON_MAJOR}.${SYS_PYTHON_MINOR}/site-packages/sentencepiece-$PKG_VERSION-py${SYS_PYTHON_MAJOR}.${SYS_PYTHON_MINOR}-linux-${ARCH}.egg/_sentencepiece.cpython-${CONDA_PY}m-${ARCH_SO_NAME}-linux-gnu.so
+fi
+
+exit 0
